9db2b33f9ff4da7df4b5a3f5a6e52c7ca37c83f0
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-03-21 17:19:53 +0600

    finish

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Contact = () => {
+    const style ={
+        textAlign:'center',
+        color:'blue'
+    }
     return (
-        <div>
-            <h4>more details call 24 Hours 16224</h4>
+        <div style={style}>
+            <h4>for more details, call 24 Hours 16224</h4>
         </div>
     );
 };
--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -3,6 +3,36 @@ import image from '../../images/image6.png'
 import './Destination.css'
 
 const Destination = () => {
+
+
+    // const tickets = [
+    //     {
+    //         from: 'Dhaka',
+    //         to: 'Brahmanbaria',
+    //         imgUrl: '../../images/tickets3.png',
+    //         ticket: 1,
+    //         ticketType: 'Single',
+    //         price: 100
+    //     },
+    //     {
+    //         from: 'Khilgoan',
+    //         to: 'Dhaka',
+    //         imgUrl: '../../images/tickets3.png',
+    //         ticket: 2,
+    //         ticketType: 'Single',
+    //         price: 1500
+    //     },
+    //     {
+    //         from: 'Brahmanbaria',
+    //         to: 'Mirpur',
+    //         imgUrl: '../../images/tickets3.png',
+    //         ticket: 3,
+    //         ticketType: 'Single',
+    //         price: 9000
+    //     }
+    // ]
+
+
     return (
         <div className="d-flex justify-content-center">
         <div className="info">
@@ -15,7 +45,10 @@ const Destination = () => {
             </form>
         </div>
         <div>
-             <img src={image} alt=""/> */
+        {/* {
+                tickets.map(ticket => <div key={ticket.ticket} ticket={ticket}></div>)
+            } */}
+             <img src={image} alt=""/>
 
         </div>
       </div>
